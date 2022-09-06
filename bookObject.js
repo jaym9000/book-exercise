ecf7d116491dbed536a444352f4ecf17dd2bd095
Jay-M <jean_marcc@hotmail.com>
2022-09-05 21:33:06 -0300

    book constructor with function inside done.

--- bookObject.js
+++ bookObject.js
@@ -2,5 +2,20 @@ function Book(title, author, numOfPages, isRead) {
   (this.title = title),
     (this.author = author),
     (this.numOfPages = numOfPages),
-    (this.isRead = isRead);
+    (this.isRead = isRead),
+    (this.info = function () {
+      return (
+        title +
+        " by " +
+        author +
+        ", " +
+        numOfPages +
+        " pages, " +
+        (isRead ? "is read" : "not read yet")
+      );
+    });
 }
+
+const getRich = new Book("GetRichOrDieTryin", "50 Cent", 250, false);
+
+console.log(getRich.info());
